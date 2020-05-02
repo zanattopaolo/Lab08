c74265a2648ccfb82ba639beb12a21f82a21f595
albertomonge <alberto.monge@polito.it>
2020-05-02 17:42:28 +0200

    Progetto base

---
+++ src/main/java/it/polito/tdp/extflightdelays/FXMLController.java
@@ -0,0 +1,51 @@
+/**
+ * Sample Skeleton for 'Scene.fxml' Controller Class
+ */
+
+package it.polito.tdp.extflightdelays;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import it.polito.tdp.extflightdelays.model.Model;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+public class FXMLController {
+
+	private Model model;
+	
+    @FXML // ResourceBundle that was given to the FXMLLoader
+    private ResourceBundle resources;
+
+    @FXML // URL location of the FXML file that was given to the FXMLLoader
+    private URL location;
+
+    @FXML // fx:id="txtResult"
+    private TextArea txtResult; // Value injected by FXMLLoader
+
+    @FXML // fx:id="distanzaMinima"
+    private TextField distanzaMinima; // Value injected by FXMLLoader
+
+    @FXML // fx:id="btnAnalizza"
+    private Button btnAnalizza; // Value injected by FXMLLoader
+
+    @FXML
+    void doAnalizzaAeroporti(ActionEvent event) {
+    	//TODO
+    }
+
+    @FXML // This method is called by the FXMLLoader when initialization is complete
+    void initialize() {
+        assert txtResult != null : "fx:id=\"txtResult\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert distanzaMinima != null : "fx:id=\"distanzaMinima\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert btnAnalizza != null : "fx:id=\"btnAnalizza\" was not injected: check your FXML file 'Scene.fxml'.";
+    }
+    
+    public void setModel(Model model) {
+    	this.model = model;
+    }
+}
